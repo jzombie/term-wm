692f9a470c6725e4ac7f83fa219ef2c55e86503f
Jeremy Harris <jeremy.harris@zenosmosis.com>
2026-06-24 16:04:29 -0500

    Update TODO

--- crates/term-wm-core/src/components/mod.rs
+++ crates/term-wm-core/src/components/mod.rs
@@ -1,4 +1,4 @@
-// TODO: Figure out how to move this to term-wm-ui-components as well
+// TODO: Rename file to `component.rs`
 use crossterm::event::Event;
 use ratatui::layout::Rect;
 use std::any::Any;
