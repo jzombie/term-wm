a7716ed2a1b05ee697205fe790fd5ef5beafe463
Jeremy Harris <jeremy.harris@zenosmosis.com>
2026-06-28 17:58:57 -0500

    Backport plumbing from future daemon

--- crates/term-wm-pty-engine/src/lib.rs
+++ crates/term-wm-pty-engine/src/lib.rs
@@ -2,6 +2,7 @@ pub mod clipboard;
 pub mod input_encoding;
 pub mod pane;
 pub mod pty;
+pub mod signal;
 pub mod title;
 
 pub use input_encoding::{ctrl_char, key_to_bytes, mouse_event_allowed, mouse_event_to_bytes};
