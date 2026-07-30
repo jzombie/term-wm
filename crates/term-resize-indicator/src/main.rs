a87daf8e34389fb844b2ed4bdc5df4ac0ccb6dce
Jeremy Harris <jeremy.harris@zenosmosis.com>
2026-07-30 07:58:42 -0500

    Remove clap

--- crates/term-resize-indicator/src/main.rs
+++ crates/term-resize-indicator/src/main.rs
@@ -1,11 +1,5 @@
-use clap::Parser;
 use term_resize_indicator::run;
 
-#[derive(Parser)]
-#[command(version, about)]
-struct Cli;
-
 fn main() {
-    Cli::parse();
     run().expect("resize indicator failed");
 }
